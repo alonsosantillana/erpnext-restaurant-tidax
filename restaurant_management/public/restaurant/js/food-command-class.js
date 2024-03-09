156b924eaf2b34a31793b14ae1f4c56b8498a30a
alonsosantillana <alonsosantillana@hotmail.com>
2024-03-09 04:28:02 -0500

    .

--- restaurant_management/public/restaurant/js/food-command-class.js
+++ restaurant_management/public/restaurant/js/food-command-class.js
@@ -6,8 +6,84 @@ class FoodCommand {
         this.render();
         RM.object(this.identifier + this.process_manage.identifier, this);
     }
-    
     render() {
+        if (!this.rendered) {
+            this.action_button = frappe.jshtml({
+                tag: "h5",
+                properties: {
+                    class: `btn btn-default btn-flat btn-food-command`,
+                    style: 'border-radius: 0 !important; font-size: 8px;'
+                },
+                content: '{{text}}<i class="fa fa-chevron-right pull-right" style="font-size: 14px; padding-top: 0px;"></i>',
+                text: this.data.process_status_data.next_action_message,
+            }).on("click", () => {
+                this.execute();
+            }, !RM.restrictions.to_change_status_order ? DOUBLE_CLICK : null)
+            // TIDAX: QUITA PERMISO A MOZOS
+            if (frappe.session.user.includes("mozo")) {
+                this.action_button.prop('disabled', true);
+            }
+
+            this.status_label = frappe.jshtml({
+                tag: "h5",
+                properties: {
+                    class: "btn btn-flat btn-food-command status-label",
+                    style: `background-color: ${this.data.process_status_data.color}; font-size: 8px;`
+                },
+                content: `<i class="${this.data.process_status_data.icon} pull-left status-label-icon" style="font-size: 9px;"></i> ${this.data.process_status_data.status_message}`,
+            });
+
+            this._time_elapsed = frappe.jshtml({
+                tag: "strong",
+                properties: {
+                    style: "font-size: 14px; left: 100%; position: sticky;"
+                },
+                content: ''
+            });
+
+            this.description = frappe.jshtml({
+                tag: "span",
+                properties: {
+                    style: "text-align: text-top; font-size: 11px;"
+                },
+                content: `${this.data.table_description} | ${this.data.short_name}`,
+            });
+
+            this.title = frappe.jshtml({
+                tag: "h5",
+                content: `${this.description.html()} ${this._time_elapsed.html()}`,
+            });
+            
+            this.item = frappe.jshtml({
+                tag: "article",
+                properties: {
+                    class: "food-command-container"
+                },
+                content: this.template
+            });
+            //TIDAX: NO MOSTRAR EN PANTALLA DELIVERYS
+            let cadena = this.data.table_description;
+            let inicio = cadena.indexOf("(") + 1;
+            let fin = cadena.indexOf(")");
+            let resultado = cadena.substring(inicio, fin);
+            let m1, m2, m3;
+            //this.usarMesas(resultado);
+            m1 = RM.restrictions.mesas_1;
+            m2 = RM.restrictions.mesas_2;
+            m3 = RM.restrictions.mesas_3;
+            if(!resultado.includes(m1) && !resultado.includes(m2) && !resultado.includes(m3)){
+            $(this.process_manage.command_container()).append(
+                this.item.html()
+            );}
+
+            this.rendered = true;
+            this.show_notes();
+
+            this.time_elapsed;
+
+        }
+    }
+    render1() {
         if (!this.rendered) {
             
             this.action_button = frappe.jshtml({
@@ -68,8 +144,12 @@ class FoodCommand {
             let inicio = cadena.indexOf("(") + 1;
             let fin = cadena.indexOf(")");
             let resultado = cadena.substring(inicio, fin);
-            
-            if(!resultado.includes("D")){
+            let m1, m2, m3;
+            //this.usarMesas(resultado);
+            m1 = RM.restrictions.mesas_1;
+            m2 = RM.restrictions.mesas_2;
+            m3 = RM.restrictions.mesas_3;
+            if(!resultado.includes(m1) && !resultado.includes(m2) && !resultado.includes(m3)){
             $(this.process_manage.command_container()).append(
                 this.item.html()
             );}
@@ -81,6 +161,7 @@ class FoodCommand {
         }
     }
 
+    
     // TIDAX: COLOR ALEATORIO
     getRandomColor() {
         const letters = '0123456789ABCDEF';
@@ -132,6 +213,48 @@ class FoodCommand {
                 RM.ready(false, "success");
             },
         });
+    }
+
+    // TIDAX: OBTENCION DE RESTRICCION DE MESAS
+    obtenerMesas(mesas) {
+        return new Promise((resolve, reject) => {
+            frappe.call({
+                method: 'frappe.client.get_value',
+                args: {
+                    doctype: 'Restaurant Settings',
+                    fieldname: mesas
+                },
+                callback: function (response) {
+                    let xxx = mesas.substring(6, 7);
+                    if (response && response.message) {
+                        let yyy = response.message['mesas_' + xxx];
+                        const mesas_1 = yyy;
+                        resolve(mesas_1);
+                    } else {
+                        reject('No se pudo obtener el valor de mesas_1.');
+                    }
+                },
+                error: function (err) {
+                    reject('Error al obtener el valor de mesas_1:', err);
+                }
+            });
+        });
+    }
+
+    async usarMesas(resultado) {
+        try {
+            const mesas_1 = await this.obtenerMesas("mesas_1");
+            const mesas_2 = await this.obtenerMesas("mesas_2");
+            const mesas_3 = await this.obtenerMesas("mesas_3");
+            if (!resultado.includes(mesas_1) && !resultado.includes(mesas_2) && !resultado.includes(mesas_3)) {
+                // Aquí puedes realizar cualquier lógica adicional
+                $(this.process_manage.command_container()).append(
+                    this.item.html()
+                );
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     remove() {
