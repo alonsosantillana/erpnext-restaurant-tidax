1d9fed4e4245812fb4f30db9a6c3006702f8571a
alonsosantillana <alonsosantillana@hotmail.com>
2024-05-30 07:48:00 -0500

    .

--- restaurant_management/api.py
+++ restaurant_management/api.py
@@ -22,7 +22,7 @@ def get_posinv_summary(from_date, to_date):
         },
         fields=["posting_date", "name", "docstatus", "status", "customer", "customer_name", 
                 "tax_id", "address_display", "currency", "net_total", "total_taxes_and_charges", "grand_total", "total", "total_amount_discount_lines",
-                "additional_discount_percentage", "discount_amount",
+                "additional_discount_percentage", "discount_amount", "codigo_tipo_documento", "tipo_comprobante", "comprobante_electronico_manual",
                 "codigo_Qr_sunat", "codigo_hash_sunat", "enlace_pdf"]
     )
 
